9b0afa6d3a597c69c0c0bab5522534cd1fe94e9d
rentainhe <596106517@qq.com>
2020-12-17 22:33:52 +0800

    delete nasnet and add shuffle net

--- models/get_network.py
+++ models/get_network.py
@@ -7,12 +7,12 @@ def get_network(args):
     if args.model == 'resnet18':
         from models.net import resnet18
         net = resnet18()
-    elif args.model == 'nasnet':
-        from models.net import nasnet
-        net = nasnet()
     elif args.model == 'mobilenet':
         from models.net import mobilenet
         net = mobilenet()
+    elif args.model == 'shufflenet':
+        from models.net import shufflenet
+        net = shufflenet()
     else:
         print('the network name you have entered is not supported yet')
         sys.exit()
--- models/net/__init__.py
+++ models/net/__init__.py
@@ -1,3 +1,3 @@
 from models.net.resnet import *
-from models.net.nasnet import *
 from models.net.mobilenet import *
+from models.net.shufflenet import *
