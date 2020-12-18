7de2adb3adbc5136500a30fe537079d20be23ddd
rentainhe <596106517@qq.com>
2020-12-18 13:46:26 +0800

    1

--- models/net/__init__.py
+++ models/net/__init__.py
@@ -1,4 +1,4 @@
-from models.net.resnet import *
+from models.net.resnet import resnet18, resnet34, resnet50
 from models.net.mobilenet import *
 from models.net.shufflenet import *
 from models.net.mobilenetv2 import *
