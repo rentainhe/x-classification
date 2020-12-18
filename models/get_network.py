55292926557bf8ea92723b7a7e941caded724349
rentainhe <596106517@qq.com>
2020-12-18 19:22:17 +0800

    debug

--- models/get_network.py
+++ models/get_network.py
@@ -31,7 +31,7 @@ def get_network(__C):
     try:
         model_path = 'models.net'
         net = getattr(import_module(model_path),__C.model)
-        return net
+        return net()
     except ImportError:
         print('the network name you have entered is not supported yet')
         sys.exit()
