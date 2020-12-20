6dd1438fc5fa0bf4d411f53b94f15591733dcdc6
rentainhe <596106517@qq.com>
2020-12-20 13:42:14 +0800

    debug

--- models/get_network.py
+++ models/get_network.py
@@ -3,30 +3,6 @@ import sys
 import importlib
 from importlib import import_module
 
-# def get_network(args):
-#     if args.model == 'resnet18':
-#         from models.net import resnet18
-#         net = resnet18()
-#     elif args.model == 'resnet34':
-#         from models.net import resnet34
-#         net = resnet34()
-#     elif args.model == 'resnet50':
-#         from models.net import resnet50
-#         net = resnet50()
-#     elif args.model == 'mobilenet':
-#         from models.net import mobilenet
-#         net = mobilenet()
-#     elif args.model == 'mobilenetv2':
-#         from models.net import mobilenetv2
-#         net = mobilenetv2()
-#     elif args.model == 'shufflenet':
-#         from models.net import shufflenet
-#         net = shufflenet()
-#     else:
-#         print('the network name you have entered is not supported yet')
-#         sys.exit()
-#     return net
-
 def get_network(__C):
     try:
         model_path = 'models.net'
