097e4d0a2d78aff5112c49027b1f3001d75d3296
rentainhe <596106517@qq.com>
2020-12-31 13:39:05 +0800

    debug

--- models/get_network.py
+++ models/get_network.py
@@ -14,16 +14,16 @@ def get_network(__C):
         print('the network name you have entered is not supported yet')
         sys.exit()
 
-class config:
-    def __init__(self):
-        self.model = 'our50'
-def count_parameters(net):
-    params = sum([param.nelement() for param in net.parameters() if param.requires_grad])
-    print("Params: %f M" % (params/1000000))
-
-c = config()
-model = get_network(c)
-# model = resnet50(num_classes=100)
-x = torch.randn(1,3,224,224)
-count_parameters(model)
-print(model(x).size())
+# class config:
+#     def __init__(self):
+#         self.model = 'our50'
+# def count_parameters(net):
+#     params = sum([param.nelement() for param in net.parameters() if param.requires_grad])
+#     print("Params: %f M" % (params/1000000))
+#
+# c = config()
+# model = get_network(c)
+# # model = resnet50(num_classes=100)
+# x = torch.randn(1,3,224,224)
+# count_parameters(model)
+# print(model(x).size())
